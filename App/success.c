6b9cf98e9cce568a12e815b457ea6493463029bd
MyeongSeok11 <myong2404@gmail.com>
2025-07-09 11:30:05 +0900

    CR-3 AllFail

--- App/success.c
+++ App/success.c
@@ -1,9 +1,2 @@
 #include <stdio.h>
-
-int main(void) {
-  int a = 0;
-  if (a = 5) { // clang-tidy: warning (assign instead of compare
-    printf("a is 5\\n");
-  }
-  return 0;
-}
+int main( ) { int x ; if (x = 5) { printf(\"%d\\n\", x ) ; } }
